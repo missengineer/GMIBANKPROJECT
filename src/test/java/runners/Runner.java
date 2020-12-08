c49b320da00fefd4f5c0496f28e2619ba928db7f
hsnbsry <67481719+hsnbsry@users.noreply.github.com>
2020-12-08 21:17:35 +0100

    Update Runner.java

--- src/test/java/runners/Runner.java
+++ src/test/java/runners/Runner.java
@@ -9,7 +9,7 @@ import org.junit.runner.RunWith;
                 "json:target/json-reports/cucumber.json",
                 "junit:target/xml-report/cucumber.xml"},
         features = "src/test/resources/features",
-        glue = "com/team11/stepdefinitions",
+        glue = "stepdefinitions",
         tags = "@",
         dryRun = true
 
